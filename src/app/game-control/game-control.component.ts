33f893d9e77d3ef3b56d160641074b9baaff6599
Alex Withington-Smith <alex@awithsmith.com>
2021-03-04 08:24:47 +0000

    added skeleton startGame() function

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  startGame() {
+    console.log('Game started');
   }
 
+  ngOnInit(): void {}
 }
